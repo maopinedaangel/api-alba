6d9c89314fe24c1f053f974c5d49fd0563526558
maopineda <ingmauriciopineda@gmail.com>
2021-11-28 12:12:01 -0500

    Creación de la API

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+api = FastAPI()
+
+
+origins = [
+    "http://localhost:8080", "https://r-store.herokuapp.com"
+]
+
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
+@api.get("/")
+async def hola_mundo():
+    return { "mensaje": "Api Alba funcionando..."} 
